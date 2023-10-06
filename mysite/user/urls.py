39240e3f1908e45a1c4ae54605c1423e070544ef
Siitrev <paplata50@gmail.com>
2023-10-06 17:05:47 +0200

    first commit

---
+++ mysite/user/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("register/", views.register_request, name="register"),
+    path("login/", views.login_request, name="login"),
+    path("logout/", views.logout_request, name="logout"),
+]
